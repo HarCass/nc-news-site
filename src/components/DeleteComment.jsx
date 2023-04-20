a2aa94edeecf616810423bec046b7e7aa7c030c1
HarCass <harry.jc.cass@gmail.com>
2023-04-20 11:43:24 +0100

    Added delete error handling

--- src/components/DeleteComment.jsx
+++ src/components/DeleteComment.jsx
@@ -3,16 +3,26 @@ import { ActiveUserContext } from "../contexts/ActiveUserContext";
 import { delCommentById } from "../api";
 
 const DeleteComment = ({author, commentId}) => {
-    const {activeUser} = useContext(ActiveUserContext)
+    const {activeUser} = useContext(ActiveUserContext);
     const [isDeleted, setIsDeleted] = useState(false);
+    const [isError, setIsError] = useState(false);
 
     const delhandler = () => {
         delCommentById(commentId)
-        .then(() => setIsDeleted(true))
+        .then(() => {
+            setIsDeleted(true);
+            setIsError(false);
+        })
+        .catch(err => {
+            setIsError(err);
+        });
     }
     
     if (isDeleted) return <h3>Comment Deleted</h3>
-    return activeUser === author ? <button onClick={delhandler}>Delete Comment</button> : null;
+    return <div className="delete-comment">
+        {activeUser === author ? <button onClick={delhandler}>Delete Comment</button> : null}
+        {isError ? <h4>Something Went Wrong With Deletion</h4> : null}
+    </div>
 }
 
 export default DeleteComment;
