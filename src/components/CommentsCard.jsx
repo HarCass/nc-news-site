3e8becf78c1214388ded6c7ee6ae7699800539d0
HarCass <harry.jc.cass@gmail.com>
2023-04-21 16:21:43 +0100

    Added single User page

--- src/components/CommentsCard.jsx
+++ src/components/CommentsCard.jsx
@@ -7,9 +7,9 @@ const CommentsCard = ({comment}) => {
     const [isDeleted, setIsDeleted] = useState(false);
 
     return <li className="comments-item" > {isDeleted ? <h3>Comment Deleted</h3> : <div className="comment-info">
-            <p className="date">{formatDate(comment.created_at)}</p>
-            <article>{comment.body}</article>
             <p>{comment.author}</p>
+            <article>{comment.body}</article>
+            <p className="date">{formatDate(comment.created_at)}</p>
             <CommentVote comment={comment}></CommentVote>
             <DeleteComment author={comment.author} commentId={comment.comment_id} setIsDeleted={setIsDeleted}></DeleteComment>
         </div>}
